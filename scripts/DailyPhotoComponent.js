46a517e5ab7e3f424ee8d5fcf1ed54b51742074d
aborgra <audrey.borgra@gmail.com>
2019-12-04 13:41:34 -0600

    added sizing to daily photo

--- scripts/DailyPhotoComponent.js
+++ scripts/DailyPhotoComponent.js
@@ -3,7 +3,7 @@ import { parsedAPIData } from "./parseAPIData.js";
 const DailyPhotoComponent = async () => {
   const dailyPhoto = await parsedAPIData();
   const contentElement = document.querySelector(".content__Photo");
-  contentElement.innerHTML += `<img src="${dailyPhoto}">`;
+  contentElement.innerHTML += `<img class="dailyPhoto" src="${dailyPhoto}">`;
 };
 
 export default DailyPhotoComponent;
